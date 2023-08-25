cddbc6cec62fb05fd479a976e015538be072b1e0
Utsavladia <124615886+Utsavladia@users.noreply.github.com>
2023-08-25 12:45:34 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Utsav</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop user interfaces<br className="sm:block hidden" />
+            I develop user interfaces <br className="sm:block hidden" />
             and web applications
           </p>
         </div>
